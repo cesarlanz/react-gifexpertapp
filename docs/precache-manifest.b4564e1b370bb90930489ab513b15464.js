6da53d0db4c48665e3246bc39d60b3bec0132bea
César Lanz <cesarlanz@gmail.com>
2020-11-11 02:01:46 -0300

    docs agregado

---
+++ docs/precache-manifest.b4564e1b370bb90930489ab513b15464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8b436622abf10c1e7a0244d1c66e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b10de866d19adfba0574",
+    "url": "/static/css/main.3d6a570d.chunk.css"
+  },
+  {
+    "revision": "288bc986db204fd0d04e",
+    "url": "/static/js/2.e837b565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e837b565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10de866d19adfba0574",
+    "url": "/static/js/main.fcd98fe3.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
